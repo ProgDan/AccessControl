8917801c863f77e8ee2d6f18e01bd09842553462
Daniel Arndt Alves <progdan@gmail.com>
2018-12-10 13:12:35 -0200

    Teste de leitura com o modulo RFID

---
+++ Components/RFID/Leitor_RFID_Raspberry_Pi.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import time
+import RPi.GPIO as GPIO
+import MFRC522
+
+# UID dos cartões que possuem acesso liberado.
+CARTOES_LIBERADOS = {
+    '4F:FD:2F:0:9D': 'Master',
+    '3C:2F:4F:0:2D': 'Teste',
+}
+
+try:
+    # Inicia o módulo RC522.
+    LeitorRFID = MFRC522.MFRC522()
+ 
+    print('Aproxime seu cartão RFID')
+ 
+    while True:
+        # Verifica se existe uma tag próxima do módulo.
+        status, tag_type = LeitorRFID.MFRC522_Request(LeitorRFID.PICC_REQIDL)
+ 
+        if status == LeitorRFID.MI_OK:
+            print('Cartão detectado!')
+ 
+            # Efetua leitura do UID do cartão.
+            status, uid = LeitorRFID.MFRC522_Anticoll()
+ 
+            if status == LeitorRFID.MI_OK:
+                uid = ':'.join(['%X' % x for x in uid])
+                print('UID do cartão: %s' % uid)
+ 
+                # Se o cartão está liberado exibe mensagem de boas vindas.
+                if uid in CARTOES_LIBERADOS:
+                    print('Acesso Liberado!')
+                    print('Olá %s.' % CARTOES_LIBERADOS[uid])
+                else:
+                    print('Acesso Negado!')
+ 
+                print('nAproxime seu cartão RFID')
+ 
+        time.sleep(.25)
+except KeyboardInterrupt:
+    # Se o usuário precionar Ctrl + C
+    # encerra o programa.
+    GPIO.cleanup()
+    print('nPrograma encerrado.')
