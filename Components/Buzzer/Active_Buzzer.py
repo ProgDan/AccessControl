8edffa38bb5b27c406965d4a253ce6177a9633ed
Daniel Arndt Alves <progdan@gmail.com>
2018-12-10 16:31:04 -0200

    Teste de buzzer

--- Components/Buzzer/Active_Buzzer.py
+++ Components/Buzzer/Active_Buzzer.py
@@ -5,35 +5,35 @@ import time
 Buzzer = 4    # pin11
 
 def setup(pin):
-	global BuzzerPin
-	BuzzerPin = pin
-	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-	GPIO.setup(BuzzerPin, GPIO.OUT)
-	GPIO.output(BuzzerPin, GPIO.HIGH)
+    global BuzzerPin
+    BuzzerPin = pin
+    GPIO.setmode(GPIO.BCM)       # Numbers GPIOs by physical location
+    GPIO.setup(BuzzerPin, GPIO.OUT)
+    GPIO.output(BuzzerPin, GPIO.HIGH)
 
 def on():
-	GPIO.output(BuzzerPin, GPIO.LOW)
+    GPIO.output(BuzzerPin, GPIO.LOW)
 
 def off():
-	GPIO.output(BuzzerPin, GPIO.HIGH)
+    GPIO.output(BuzzerPin, GPIO.HIGH)
 
 def beep(x):
-	on()
-	time.sleep(x)
-	off()
-	time.sleep(x)
+    on()
+    time.sleep(x)
+    off()
+    time.sleep(x)
 
 def loop():
-	while True:
-		beep(0.5)
+    while True:
+        beep(0.5)
 
 def destroy():
-	GPIO.output(BuzzerPin, GPIO.HIGH)
-	GPIO.cleanup()                     # Release resource
+    GPIO.output(BuzzerPin, GPIO.HIGH)
+    GPIO.cleanup()                     # Release resource
 
 if __name__ == '__main__':     # Program start from here
-	setup(Buzzer)
-	try:
-		loop()
-	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		destroy()
+    setup(Buzzer)
+    try:
+        loop()
+    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+        destroy()
