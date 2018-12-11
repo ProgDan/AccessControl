e1b86eeb65d8e74efef078d0be181da388a887dd
Daniel Arndt Alves <progdan@gmail.com>
2018-12-11 17:45:39 -0200

    Create Database Structure

---
+++ DB/Scripts/Table_Schema.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS Movimento;
+
+CREATE TABLE Movimento (
+	MovID INTEGER PRIMARY KEY,
+	MovData INTEGER NOT NULL,
+	MovBarra TEXT NOT NULL,
+	MovSentido TEXT NOT NULL,
+	MovBloqueado INTEGER DEFAULT 0 NOT NULL,
+	MovForaHorario INTEGER DEFAULT 0 NOT NULL,
+	MovProvisorio INTEGER DEFAULT 0 NOT NULL,
+	UsrCodigo INTEGER,
+	FOREIGN KEY(UsrCodigo) REFERENCES Usuario(UsrCodigo)
+);
+
+DROP TABLE IF EXISTS Usuario;
+
+CREATE TABLE Usuario (
+	UsrCodigo INTEGER NOT NULL PRIMARY KEY,
+	UsrNome TEXT NOT NULL,
+	UsrBarra TEXT,
+	UsrProvisorio TEXT,
+	UsrProvisorioValidade INTEGER
+);
+
+DROP TABLE IF EXISTS Horario;
+
+CREATE TABLE Horario (
+	HorID INTEGER PRIMARY KEY,
+	HorDia INTEGER NOT NULL,
+	HorInicio TEXT NOT NULL,
+	HorFim TEXT NOT NULL,
+	HorSentido TEXT NOT NULL,
+	UsrCodigo INTEGER NOT NULL,
+	FOREIGN KEY(UsrCodigo) REFERENCES Usuario(UsrCodigo)
+);
