8f626e4518c0691cf221f42e4962f7f2e5747937
Daniel Arndt Alves <progdan@gmail.com>
2018-12-12 09:18:58 -0200

    Criação da rotina de interação com o leitor de RFID no aplicativo

--- RFID/MFRC522.py
+++ RFID/MFRC522.py
@@ -27,7 +27,7 @@ import signal
 import time
   
 class MFRC522:
-  NRSTPD = 22
+  NRSTPD = 6  # Board Pin 22
   
   MAX_LEN = 16
   
@@ -129,7 +129,7 @@ class MFRC522:
   
   def __init__(self, dev='/dev/spidev0.0', spd=1000000):
     spi.openSPI(device=dev,speed=spd)
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(self.NRSTPD, GPIO.OUT)
     GPIO.output(self.NRSTPD, 1)
     self.MFRC522_Init()
