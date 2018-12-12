c35bff07b9df69dcb43b2bdab2da92b265e55b32
Daniel Arndt Alves <progdan@gmail.com>
2018-12-12 09:05:28 -0200

    Criação da rotina de interação com o leitor de RFID no aplicativo

--- AccessControl.py
+++ AccessControl.py
@@ -23,11 +23,11 @@ s.connect((gw[2], 0))
 ipaddr = s.getsockname()[0]
 
 # Pinos LCD x Raspberry (GPIO)
-lcd_rs        = 35
-lcd_en        = 33
-lcd_d4        = 25
-lcd_d5        = 23
-lcd_d6        = 22
+lcd_rs        = 18
+lcd_en        = 23
+lcd_d4        = 12
+lcd_d5        = 16
+lcd_d6        = 20
 lcd_d7        = 21
 lcd_backlight = 4
 
@@ -36,7 +36,6 @@ lcd_colunas = 16
 lcd_linhas  = 2
 
 # Inicializa o LCD nos pinos configurados acima
-GPIO.setmode(GPIO.BOARD)
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5,
                            lcd_d6, lcd_d7, lcd_colunas, lcd_linhas,
                            lcd_backlight)
@@ -58,7 +57,7 @@ lcd.set_cursor(3,0)
 lcd.message('CDA CONTROL')
 
 def setup():
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BCM)
 
 def destroy():
     GPIO.cleanup()
